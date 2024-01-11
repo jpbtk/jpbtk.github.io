d9fe480f5997d1607704c271b96ca4307033434b
TK. <113032465+jpbtk@users.noreply.github.com>
2024-01-11 13:39:59 +0000

    configjs修正修正

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
   },
-}
+};
 
 module.exports = nextConfig
