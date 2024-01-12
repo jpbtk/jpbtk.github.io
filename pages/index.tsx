af99e2efeca15c5ac4b5402244a30bd95740555b
TK <113032465+jpbtk@users.noreply.github.com>
2024-01-12 09:23:16 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,10 @@ export default function Home() {
                         Get started by editing&nbsp;
                         <code className={styles.code}>pages/index.tsx</code>
                     </p>
+                    <p>
+                        testtesttetststststts
+                        <code className={styles.code}>dlfjldj</code>
+                    </p>
                     <div>
                         <a
                             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
