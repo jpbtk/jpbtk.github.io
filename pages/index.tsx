13fc92e2490e97db478e2e918dda8e2ebd7eb06e
TK <113032465+jpbtk@users.noreply.github.com>
2024-01-13 00:00:10 +0900

    ヘッダー動けえええええ

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
     return (
         <div>
+            <Header />
             <main className={styles.main}>
                 <div className={styles.description}>
                     <p>
