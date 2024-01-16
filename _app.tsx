809974c944aaa7c0c00fd98c10ffef9007a63848
TK <113032465+jpbtk@users.noreply.github.com>
2024-01-16 14:54:56 +0900

    Update _app.tsx

--- _app.tsx
+++ _app.tsx
@@ -1,6 +1,9 @@
+import { AnimatePresence } from 'framer-motion'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  <AnimatePresence mode="wait">
+    return <Component key={router.asPath} {...pageProps} />
+  </AnimatePresence>
 }
